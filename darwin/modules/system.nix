8594bf7c99481296b36b848a3215e36f5dafc45c
neodymium6 <104201402+neodymium6@users.noreply.github.com>
2026-01-09 17:06:34 +0900

    Add nix-darwin configuration with modular structure

---
+++ darwin/modules/system.nix
@@ -0,0 +1,50 @@
+{ username, ... }:
+
+{
+  system.defaults.dock = {
+    autohide = true;
+    show-recents = false;
+    tilesize = 40;
+    mineffect = "scale";
+    persistent-apps = [
+      { app = "/System/Applications/Mail.app"; }
+      { app = "/System/Applications/System Settings.app"; }
+    ];
+  };
+
+  system.defaults.finder = {
+    AppleShowAllExtensions = true;
+    ShowStatusBar = true;
+    ShowPathbar = true;
+    _FXShowPosixPathInTitle = true;
+    FXEnableExtensionChangeWarning = false;
+
+    ShowHardDrivesOnDesktop = false;
+    ShowExternalHardDrivesOnDesktop = true;
+    ShowRemovableMediaOnDesktop = true;
+    ShowMountedServersOnDesktop = false;
+
+    NewWindowTarget = "Other";
+    NewWindowTargetPath = "file:///Users/${username}/";
+  };
+
+  system.defaults.NSGlobalDomain = {
+    AppleShowAllExtensions = true;
+    AppleShowAllFiles = true;
+  };
+
+  system.defaults.CustomUserPreferences = {
+    "com.apple.inputmethod.Kotoeri" = {
+      "JIMPrefLiveConversionKey" = false;
+    };
+    NSGlobalDomain = {
+      "com.apple.mouse.linear" = true;
+    };
+    "com.apple.finder" = {
+      "FXPreferredViewStyle" = "clmv";
+    };
+  };
+
+  system.defaults.WindowManager.EnableStandardClickToShowDesktop = false;
+  system.defaults.".GlobalPreferences"."com.apple.mouse.scaling" = 3.0;
+}
