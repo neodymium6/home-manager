f48d26fc09397971dd7e303f4c5f430a8b7ecfa1
neodymium6 <104201402+neodymium6@users.noreply.github.com>
2026-01-09 17:32:35 +0900

    Remove unnecessary long comments

--- home.nix
+++ home.nix
@@ -11,14 +11,7 @@ in
   home.homeDirectory =
     if isDarwin then "/Users/${username}" else "/home/${username}";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "25.05"; # Please read the comment before changing.
+  home.stateVersion = "25.05";
 
   home.packages = with pkgs; [
     # Shell & Prompt
@@ -62,8 +55,6 @@ in
     wezterm
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
     # Common dotfiles (Linux & macOS)
     ".config/tmux/tmux.conf" = {
@@ -120,26 +111,8 @@ in
     fi
   '';
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/neodymium6/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = { EDITOR = "nvim"; };
 
-  # Let Home Manager install and manage itself.
-  # programs.home-manager.enable = true;
   programs = {
     home-manager.enable = true;
     bash = {
