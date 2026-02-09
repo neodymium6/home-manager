f792261dc13a40852acf4aa01ac742210de60a00
neodymium6 <104201402+neodymium6@users.noreply.github.com>
2026-02-09 20:41:30 +0900

    Improve Chrome new window script

--- config/raycast/script-commands/open-chrome-new-window.sh
+++ config/raycast/script-commands/open-chrome-new-window.sh
@@ -6,9 +6,39 @@
 # @raycast.mode silent
 # @raycast.packageName Browser
 
-osascript <<'APPLESCRIPT'
-tell application "Google Chrome"
-  activate
-  make new window
+osascript <<'APPLESCRIPT' >/dev/null
+set appName to "Google Chrome"
+
+if application appName is not running then
+  tell application appName
+    activate
+    delay 0.1
+    make new window
+  end tell
+  return
+end if
+
+set hasWindowHere to false
+tell application "System Events"
+  try
+    set hasWindowHere to ((count (windows of process appName where value of attribute "AXMinimized" is false)) > 0)
+  end try
 end tell
+
+if hasWindowHere then
+  tell application appName
+    activate
+    make new window
+  end tell
+else
+  tell application appName to make new window
+
+  tell application "System Events"
+    repeat until (count (windows of process appName where value of attribute "AXMinimized" is false)) > 0
+      delay 0.05
+    end repeat
+  end tell
+
+  tell application appName to activate
+end if
 APPLESCRIPT
