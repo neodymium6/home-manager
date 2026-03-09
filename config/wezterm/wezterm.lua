59d594fdd6a0d098f8242560d6915843e2350e8f
neodymium6 <104201402+neodymium6@users.noreply.github.com>
2026-03-09 19:10:31 +0900

    Disable WezTerm window close confirmation

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -4,6 +4,7 @@ local config = wezterm.config_builder()
 
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
+config.window_close_confirmation = "NeverPrompt"
 
 config.font = wezterm.font_with_fallback({
 	"Hack Nerd Font Mono",
